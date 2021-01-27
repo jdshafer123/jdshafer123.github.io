ddfeac54f697c185e48499de4ac9ab3430d34f49
jdshafer123 <72616547+jdshafer123@users.noreply.github.com>
2021-01-27 10:27:24 -0500

    Update canvas-script.js

--- canvas-script.js
+++ canvas-script.js
@@ -1,10 +1,9 @@
-// Game Object
-  var flappyMonster = new FlappyMonster(canvas);
-  flappyMonster.start();
-
 window.onload = function() {
 
   var canvas = document.getElementById('flappy-monster-game');
+// Game Object
+  var flappyMonster = new FlappyMonster(canvas);
+  flappyMonster.start();
 
 
 };
